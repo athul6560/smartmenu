40cc53cab5977a97b3bbd620f60ba3ed2a01a907
Android1 <parezadroid1@parezaonline.com>
2021-02-26 17:53:36 +0530

    dashboard design

--- app/src/main/java/com/andria/qrscan/Activities/WelcomeActivity.java
+++ app/src/main/java/com/andria/qrscan/Activities/WelcomeActivity.java
@@ -14,7 +14,6 @@ import com.andria.qrscan.Model.ItemModel;
 import com.andria.qrscan.R;
 
 
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -23,9 +22,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WelcomeActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 5000;
-    ItemDetailModel item;
-    List<ItemDetailModel> itemDetailModel = new ArrayList<>();
+    private static int SPLASH_TIME_OUT = 2000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,31 +31,7 @@ public class WelcomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
 
 
-        String json = SmartMenuUtil.getItem(WelcomeActivity.this);
-        String formattedjson = json.replaceAll("\\s", "");
-
-        try {
-            JSONObject mainObject = new JSONObject(formattedjson);
-            JSONArray array = mainObject.getJSONArray("Items");
-            for (int i = 0; i < array.length(); i++) {
-                item = new ItemDetailModel();
-                JSONObject itemobject = array.getJSONObject(i);
-                item.setCategory(itemobject.getString("category"));
-                item.setDishName(itemobject.getString("dishName"));
-                item.setImage_url(itemobject.getString("image_url"));
-                item.setOffer_price(itemobject.getInt("offer_price"));
-                item.setPrice(itemobject.getInt("price"));
-                itemDetailModel.add(item);
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        Toast.makeText(this, ""+itemDetailModel.get(0).getCategory(), Toast.LENGTH_SHORT).show();
-
-      /*  for(int i=0;i<itemDetailModel.size();i++){
-            System.out.println("size:-"+itemDetailModel.get(i).getCategory());
-        }*/
-        /*new Handler().postDelayed(new Runnable() {
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent i2 = new Intent(WelcomeActivity.this, DashboardActivity.class);
@@ -65,7 +39,6 @@ public class WelcomeActivity extends AppCompatActivity {
                 finish();
             }
         }, SPLASH_TIME_OUT);
-*/
 
     }
 }
