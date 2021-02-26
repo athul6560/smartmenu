d39ba9ad12891b2f22f4732f2f783a0511cec8fa
Athul <parezadroid2@parezaonline.com>
2021-02-26 18:16:32 +0530

    null point fixed

--- app/src/main/java/com/andria/qrscan/Activities/WelcomeActivity.java
+++ app/src/main/java/com/andria/qrscan/Activities/WelcomeActivity.java
@@ -23,36 +23,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WelcomeActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 5000;
-    ItemDetailModel item;
-    List<ItemDetailModel> itemDetailModel = new ArrayList<>();
+
+
+    List<ItemDetailModel> itemDetailModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-
+        itemDetailModel = new ArrayList<>();
         String json = SmartMenuUtil.getItem(WelcomeActivity.this);
         String formattedjson = json.replaceAll("\\s", "");
 
         try {
             JSONObject mainObject = new JSONObject(formattedjson);
             JSONArray array = mainObject.getJSONArray("Items");
+
             for (int i = 0; i < array.length(); i++) {
-                item = new ItemDetailModel();
+
+                ItemDetailModel item = new ItemDetailModel();
+
                 JSONObject itemobject = array.getJSONObject(i);
+
+
                 item.setCategory(itemobject.getString("category"));
                 item.setDishName(itemobject.getString("dishName"));
                 item.setImage_url(itemobject.getString("image_url"));
-                item.setOffer_price(itemobject.getInt("offer_price"));
+
+
                 item.setPrice(itemobject.getInt("price"));
+
                 itemDetailModel.add(item);
             }
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        Toast.makeText(this, ""+itemDetailModel.get(0).getCategory(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, ""+itemDetailModel.size(), Toast.LENGTH_SHORT).show();
 
       /*  for(int i=0;i<itemDetailModel.size();i++){
             System.out.println("size:-"+itemDetailModel.get(i).getCategory());
